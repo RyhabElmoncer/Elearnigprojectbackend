39c94de597f89810a20446dbcd322b9acc6bda58
ryhab elmoncer <ryhabelmoncerr@gmail.com>
2025-07-17 19:26:17 +0100

    fist commit

---
+++ src/test/java/com/org/ElearnigProject/ElearnigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.ElearnigProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearnigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
